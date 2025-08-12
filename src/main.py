4ff74d0788a10413373e478e04041f520e680ab1
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2025-08-11 21:41:33 -0500

    Add global configuration file.
    - Add global.yaml file to control running different algorithms.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os 
+import yaml
 import time
 import logging
 from datetime import datetime
@@ -13,6 +14,21 @@ log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(filename=log_filename, level=logging.INFO, format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger(__name__) 
 
+# Load the global configuration file
+global_conf = os.path.join(os.path.dirname(os.getcwd()), 'config', 'global.yaml')
+
+try:
+    with open(global_conf, "r") as f:
+        config = yaml.safe_load(f)
+
+except FileNotFoundError:
+    logger.error(f"Global configuration file not found in: {global_conf}")
+    raise FileNotFoundError(f"Global configuration file not found: {global_conf}")
+
+except yaml.YAMLError as e:
+    logger.error(f"Error parsing YAML: {e}")
+
+
 # Import the internal modules
 from no_gating import no_gating
 from hard_gating import hard_gating
@@ -20,3 +36,5 @@ from soft_gating import soft_gating
 from imoco import imoco
 from mocostorm import mocostorm
 from xdgrasp import xdgrasp
+
+
