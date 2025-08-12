da4d61a116148dcc62cec19b333ff1361cc91f4c
Sabbir Ahmed <sabbirahmed.cuet.ete@gmail.com>
2025-08-11 22:34:49 -0500

    Complete setup for doing recons using global config file.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import os 
 import yaml
 import time
+import argparse
 import logging
 from datetime import datetime
 
@@ -14,27 +15,47 @@ log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
 logging.basicConfig(filename=log_filename, level=logging.INFO, format=log_format, datefmt="%Y-%m-%d %H:%M:%S")
 logger = logging.getLogger(__name__) 
 
-# Load the global configuration file
-global_conf = os.path.join(os.path.dirname(os.getcwd()), 'config', 'global.yaml')
 
-try:
-    with open(global_conf, "r") as f:
-        config = yaml.safe_load(f)
+def main(config_path):
+    logger.info(f"Loading the global config file from: {config_path}.")
+    
+    try:
+        with open(config_path, "r") as f:
+            config = yaml.safe_load(f)
 
-except FileNotFoundError:
-    logger.error(f"Global configuration file not found in: {global_conf}")
-    raise FileNotFoundError(f"Global configuration file not found: {global_conf}")
+    except FileNotFoundError:
+        logger.error(f"Global configuration file not found in: {global_conf}")
+        raise FileNotFoundError(f"Global configuration file not found: {global_conf}")
 
-except yaml.YAMLError as e:
-    logger.error(f"Error parsing YAML: {e}")
+    except yaml.YAMLError as e:
+        logger.error(f"Error parsing YAML: {e}")
+
+    # Run the moco-recons 
+    if config['reconstructions']['no_gating']:
+        from no_gating import no_gating
+
+    if config['reconstructions']['hard_gating']:
+        from hard_gating import hard_gating
+
+    if config['reconstructions']['soft_gating']:
+        from soft_gating import soft_gating
+
+    if config['reconstructions']['xdgrasp']:
+        from xdgrasp import xdgrasp
+    
+    if config['reconstructions']['imoco']:
+        from imoco import imoco
+
+    if config['reconstructions']['mocostorm']:
+        from mocostorm import mocostorm
 
 
-# Import the internal modules
-from no_gating import no_gating
-from hard_gating import hard_gating
-from soft_gating import soft_gating
-from imoco import imoco
-from mocostorm import mocostorm
-from xdgrasp import xdgrasp
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Reconstruct image using motion compensated algorithms."
+        )
+    parser.add_argument("--config_path", type=str)
 
+    args = parser.parse_args()
+    main(args.config_path)
